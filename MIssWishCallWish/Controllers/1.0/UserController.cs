d39f5b9e5553e172c94af408b53b54622053902b
Nikhil1292 <nikhilt@alohatechnology.com>
2016-05-27 00:42:18 +0530

    API : Remove value controller 1. Added user conrtroller. 2. Change layout.

---
+++ MIssWishCallWish/Controllers/1.0/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MIssWishCallWish.Controllers._1._0
+{
+    public class UserController : ApiController
+    {
+        [Route("api/user")]
+        public string Getusers()
+        {
+            return "User controller";
+        }
+    }
+}
